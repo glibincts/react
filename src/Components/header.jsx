7863082012ba0380fd0eeeb57992fb1361786374
Libinz <libingeorge3@gmail.com>
2020-04-25 18:53:19 +0530

    added header2

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,18 +5,20 @@ class Header extends Component {
   render() {
     return (
       <div className="mainHeader bg-primary text-white">
-        <div className="container-fluid flex-column">
+        <div className="container-fluid flex-column header-container">
           <div className="row p-2">
             <div className="row col-sm-4">
-              <div className="logo-content">
-                <span>ReactApp</span>
+              <div className="logo-content align-middle">
+                <span className="align-middle logo-text font-weight-bolder">
+                  ReactApp
+                </span>
               </div>
             </div>
             <div className="row col-sm-8">
-              <div className="navBar-content">
-                <ul className="list-inline" id="navId">
-                  <li>Home</li>
-                  <li>About</li>
+              <div className="navBar-content float-right">
+                <ul className="nav">
+                  <li className="nav-link active">Home</li>
+                  <li className="nav-link active">About</li>
                 </ul>
               </div>
             </div>
