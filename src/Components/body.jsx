57a7481463a5aa2c3a8cc5893d2a5e4ae312d4dc
libin <libingeorge3@gmail.com>
2020-05-03 16:45:11 +0530

    new

--- src/Components/body.jsx
+++ src/Components/body.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
+import reactImage from '../Imges/react.jpg';
 
 class Body extends Component {
   state = {};
   render() {
-    return <h1>Body Called1</h1>;
+    return (
+      <div className="container" id='container1'>
+        <div className="row text-center" id='row1'>
+          <div className="col-sm-6 text-center" >
+            <h4>Welcome to MyApp</h4> 
+            <p>Hey guys, thanks for watching my new React WebApplication. This is my first project in react webapp</p>
+          </div>
+          <div className="col-sm-6 text-center">
+              <img src={reactImage} alt="Responsive image" className='img-thumbnail' width='50%'/>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
